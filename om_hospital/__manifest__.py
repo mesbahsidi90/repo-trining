8c39d135f50f7b8f4eb11047918a7cc67ca6bb8d
Mesbah Sidi <mesbahzin89@gmail.com>
2023-04-10 16:31:43 +0300

    first module

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Hospital Managment',
+    'description': "Hospital Managment System",
+    'catogory': 'Hospital',
+    'author': 'Odoo Matea',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'sequence': -100,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
